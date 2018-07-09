194912fe9a2d7707fc88974731e0391bbf302fe8
Michael Surya <tomichaels@gmail.com>
2018-07-09 17:27:25 +0000

    added callback function

---
+++ HTMLcallback.js
@@ -0,0 +1,35 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+    var buffered = '';
+
+    var requestOptions = {
+      host: options.host,
+      path: options.path
+    };
+  
+    https.get(requestOptions, function (response) {
+      response.setEncoding('utf8');
+      callback;
+  
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      buffered += data;
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(buffered);
+    });
+    });
+  }
+  
+  function printHTML (html) {
+    console.log(html);
+  }
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+getHTML(requestOptions, printHTML);
